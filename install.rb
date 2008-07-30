1c7670ad1cace9b267c0943032c0d05682ac05ff
Nigel Ramsay <nigelramsay@nigel-ramsays-macbook.local>
2008-07-31 06:42:10 +1200

    retry path

--- install.rb
+++ install.rb
@@ -1,2 +1,2 @@
 puts IO.read(File.join(File.dirname(__FILE__), 'README'))
-FileUtils.copy('test/fixtures_test.rb', RAILS_ROOT + "/test/units/fixtures_test.rb", :verbose => true)
+FileUtils.copy(Dir[File.dirname(__FILE__) + 'test/fixtures_test.rb', RAILS_ROOT + "/test/units/fixtures_test.rb", :verbose => true)
